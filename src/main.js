50be01445ccf47fd03aac229ed64a9cb121539fd
박진희 <melbazzang@naver.com>
2023-07-23 15:42:07 +0900

    [Fix] vue-router export처리

--- src/main.js
+++ src/main.js
@@ -6,7 +6,10 @@ import "bootstrap/dist/js/bootstrap";
 import App from "./App.vue";
 import BaseCard from "./components/UI/BaseCard.vue";
 
+// 생성한 뷰 라우터 받아오기
+import { router } from './router/index.js';
+
 const app = createApp(App);
 app.component("base-card", BaseCard);
-
+app.use(router);
 app.mount("#app");
