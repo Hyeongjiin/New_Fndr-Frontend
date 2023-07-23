ae0ad965be9a36a4a6fbd9da53269e7635333872
박진희 <melbazzang@naver.com>
2023-07-23 15:41:02 +0900

    [Add] 메인페이지vue파일 추가

---
+++ src/components/mainpage/MainPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <search-section></search-section>
+    <Main-card-list></Main-card-list>
+</template>
+
+
+<script>
+import MainCardList from "../list-resources/MainCardList.vue";
+import SearchSection from "../mainpage/SearchSection.vue";
+
+export default {
+    components: {
+        MainCardList,
+        SearchSection,
+    }
+}
+
+</script>
