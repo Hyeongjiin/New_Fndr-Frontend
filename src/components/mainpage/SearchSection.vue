4bea9f930c8b6a7120a6161598a724fd1b5d53c0
박진희 <melbazzang@naver.com>
2023-07-26 21:23:53 +0900

    [Style] style시트의 html 수정

--- src/components/mainpage/SearchSection.vue
+++ src/components/mainpage/SearchSection.vue
@@ -121,12 +121,6 @@ input::placeholder {
   width: 50%;
 }
 
-html {
-  -webkit-touch-callout: none;
-  -webkit-user-select: none;
-  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-}
-
 .button-box {
   display: flex;
   justify-content: center;
