76ce0e846e69ac808dc514e01c6adef41a110be1
염정연 <yeomi6185@gmail.com>
2023-07-30 04:08:32 +0900

    [Add] SearchList 컴포넌트 추가

---
+++ src/components/recruit/list/SearchList.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="box1">
+        <div class="maintext">
+        Company Name
+        </div>
+
+        <div class="smallbox">
+        <h5 class="card-title">Design a scalable, stable and secure platform using cloud platforms, especially AWS, and key technologies such
+            as kubernetes and Kafka...</h5>
+        <a href="#" class="btn btn-primary">TEST BUTTON</a>
+        </div>
+      </div>
+    
+</template>
+
+<style scoped>
+
+    .box1{
+        
+        margin-left:2%;
+        margin-bottom:3%;
+        text-align: center;
+        width : 700px;
+        height: auto;
+        background-color: rgba(248, 248, 248, 1);
+        border-radius:  30px;;    
+    }
+        .maintext{
+        font-size: 25px;
+    }
+    .box2{
+        background-color:rgba(227, 227, 227, 1);
+    }
+    .card-title{
+        font-size: 22px;
+    }
+
+</style>
