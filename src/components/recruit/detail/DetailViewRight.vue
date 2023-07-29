51c2da23b11d4b7ffaa8555f32eb199390433d8e
염정연 <yeomi6185@gmail.com>
2023-07-29 21:55:37 +0900

    [Fix] detail 컴포넌트  css 수정, html 수정

--- src/components/recruit/detail/DetailViewRight.vue
+++ src/components/recruit/detail/DetailViewRight.vue
@@ -1,6 +1,6 @@
     <template>
-
-      
+ 
+      <div class ="rightbox">
 
         <img src="img.jpg" alt="company picture" style="width:173px">
         
@@ -20,21 +20,27 @@
               <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
             </svg> <span>website</span><br>
 
-        </section>
+          </section>
 
-       <p><button>Contact</button></p>
+         <button>Contact</button>
 
+      </div>
     
-
-   
- 
 </template>
 
 
 <style scoped>
 
-    .textbox{
-        line-height:30px;
+    .rightbox{
+
+      margin-top: 10%;
+      margin-left: 10px;
+      border: 1px solid;
+      height: 300px;
+      width: auto;
+      text-align: center;
+      padding: 15px;
+
     }
 
     .button-box{
@@ -43,15 +49,15 @@
         align-items : center;
         margin-top : 26px;
         width: 268px;   
-        margin:0 auto;
+        margin: 0 auto;
         margin-top: 26px;
       }
 
       .card {
-        text-align: center;
         width: 378px; 
         height: 604px; 
         margin-bottom: 20px;   
+        margin-right: 32px;
       }
 
       h1{
