d4f30a668bcd154ff25c88c964d00128243c976f
염정연 <yeomi6185@gmail.com>
2023-07-29 19:17:27 +0900

    [Add] DetailPage 추가

---
+++ src/components/recruit/detail/DetailPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <detail-view-left></detail-view-left>
+    <detail-view-right></detail-view-right>
+</template>
+
+
+<script>
+import DetailViewLeft from "./DetailViewLeft.vue";
+import DetailViewRight from "./DetailViewRight.vue";
+
+export default {
+    components: {
+        DetailViewLeft,
+        DetailViewRight,
+    }
+}
+
+</script>
