98b09b63257ff791d2665a77036b79ba752dfd7c
염정연 <yeomi6185@gmail.com>
2023-07-29 21:54:18 +0900

    [Fix] detailpage 컴포넌트들 css 적용

--- src/components/recruit/detail/DetailPage.vue
+++ src/components/recruit/detail/DetailPage.vue
@@ -1,18 +1,27 @@
 <template>
+    <div class="box">
     <detail-view-left></detail-view-left>
     <detail-view-right></detail-view-right>
+    </div>
 </template>
 
+<style scoped>
+.box{
+    display: flex;
+}
+
+</style>
 
 <script>
 import DetailViewLeft from "./DetailViewLeft.vue";
 import DetailViewRight from "./DetailViewRight.vue";
 
 export default {
+
     components: {
         DetailViewLeft,
         DetailViewRight,
     }
+    
 }
-
 </script>
