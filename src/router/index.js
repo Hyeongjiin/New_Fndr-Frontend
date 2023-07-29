fb517b13ee21fd4f9f7ddd63923f8bbc085d51d9
박진희 <melbazzang@naver.com>
2023-07-29 22:55:35 +0900

    [Fix] router의 index.js에 SearchPage 경로 추가

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,14 @@ import { createRouter, createWebHistory } from "vue-router";
 
 // 만들어둔 컴포넌트 임포트 시키기
 import MainPage from "../components/mainpage/MainPage.vue";
+import SearchPage from "../components/recruit/list/SearchPage.vue";
 import DetailPage from "../components/recruit/detail/DetailPage.vue";
 
 // 라우터 설계
 const routes = [
   { path: "/", component: MainPage },
   { path: "/detail", component: DetailPage },
+  { path: "/search-jobs", component: SearchPage },
 
 ];
 
