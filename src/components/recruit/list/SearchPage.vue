66ab3d0a5d8f39517f506cf08f96749b3c978918
염정연 <yeomi6185@gmail.com>
2023-07-30 04:09:40 +0900

    [Add] SearchPage 컴포넌트 추가

--- src/components/recruit/list/SearchPage.vue
+++ src/components/recruit/list/SearchPage.vue
@@ -1,15 +1,35 @@
 <template>
+    <div class="box">
     <search-side-bar></search-side-bar>
+    <div class="box2">
+    <search-list></search-list>
+    <search-list></search-list>
+    <search-list></search-list>
+    <search-list></search-list>
+    </div>
+</div>
 </template>
+
+<style scoped>
+.box{
+    display: flex;
+    margin-top: 3%;
+}
+
+</style>
 
 
 <script>
 import SearchSideBar from "../list/SearchSideBar.vue";
+import SearchList from "../list/SearchList.vue";
+
 
 export default {
     components: {
         SearchSideBar,
+        SearchList,
     }
 }
 
 </script>
+
