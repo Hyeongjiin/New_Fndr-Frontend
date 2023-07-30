38e127b91d43bf49789fda2bf64b85fa5e9987a8
염정연 <yeomi6185@gmail.com>
2023-07-31 03:19:06 +0900

    [Fix] SearchList css , html 수정

--- src/components/recruit/list/SearchList.vue
+++ src/components/recruit/list/SearchList.vue
@@ -1,13 +1,20 @@
 <template>
+
     <div class="box1">
+
         <div class="maintext">
-        Company Name
+        Remote Client Specialist 
         </div>
 
+        <div class="middlebox">
+            <div style="margin-right: 15px;"> Agent Pronto  </div>
+            <div style="margin-right: 15px;">  reloction or remote  </div>
+            <div style="margin-right: 15px;"> visa support  </div>
+        </div>
+        
         <div class="smallbox">
         <h5 class="card-title">Design a scalable, stable and secure platform using cloud platforms, especially AWS, and key technologies such
             as kubernetes and Kafka...</h5>
-        <a href="#" class="btn btn-primary">TEST BUTTON</a>
         </div>
       </div>
     
@@ -17,22 +24,31 @@
 
     .box1{
         
-        margin-left:2%;
+        margin-left:3%;
         margin-bottom:3%;
-        text-align: center;
+        padding:30px;
         width : 700px;
         height: auto;
         background-color: rgba(248, 248, 248, 1);
         border-radius:  30px;;    
     }
         .maintext{
-        font-size: 25px;
+        font-size: 30px;
     }
-    .box2{
-        background-color:rgba(227, 227, 227, 1);
-    }
+   .middlebox{
+    display: flex;
+   }
+  
     .card-title{
         font-size: 22px;
+    }
+    .smallbox{
+        border: 10px;
+        margin-top: 25px;
+        background-color: rgba(217, 217, 217, 1);
+        border-radius: 10px;
+        display:inline-block;
+        text-align: center;
     }
 
 </style>
