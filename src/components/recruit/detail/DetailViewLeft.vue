adabbc8754baa55e5e98c7975f10c6ded428602f
염정연 <yeomi6185@gmail.com>
2023-07-31 04:29:13 +0900

    [Fix] DetailViewRight CSS HTML 수정

--- src/components/recruit/detail/DetailViewLeft.vue
+++ src/components/recruit/detail/DetailViewLeft.vue
@@ -1,64 +1,88 @@
 <template>
   <div class="card">
-    <h1 class="title"> 채용공고 제목(직무명) </h1> <br>
-    
+
+    <h1 class="title"> 채용공고 제목(직무명) </h1> 
+
+    <div class="stackbox">
+      <div class="stack">Java</div>
+      <div class="stack">Python</div>
+      <div class="stack">c++</div>
+      <div class="stack">C#</div>
+    </div>
+
     <hr style="margin-top: 0; ">
 
     <section>
       <span>about company</span>
-        <br>
-        <br>
-          <div class="companyinformantion">
-            We provide a simple and clear payment API, which enables webshop and app builders to implement more than ten
-            different payment methods in one go.In the first
-            few years of existence, Mollie was a developer-only adventure and about 15 years later devs and techs still
-            comprise roughly 30% of all staff.
-            As a natural consequence, Mollie still is—and aims to remain—highly tech-minded. We take a developers-first
-            approach to our products and internal
-            development strategy, something that both our customers and our developers appreciate.
-          </div>
-        <br>
-        <br>
+      <br>
+      <br>
+      <div class="companyinformantion">
+        We provide a simple and clear payment API, which enables webshop and app builders to implement more than ten
+        different payment methods in one go.In the first
+        few years of existence, Mollie was a developer-only adventure and about 15 years later devs and techs still
+        comprise roughly 30% of all staff.
+        As a natural consequence, Mollie still is—and aims to remain—highly tech-minded. We take a developers-first
+        approach to our products and internal
+        development strategy, something that both our customers and our developers appreciate.
+      </div>
+      <br>
+      <br>
     </section>
-
 
 
     <section>
       <span>about company</span>
-        <br>
-        <br>
-          <div class="companyinformantion">
-            We provide a simple and clear payment API, which enables webshop and app builders to implement more than ten
-            different payment methods in one go.In the first
-            few years of existence, Mollie was a developer-only adventure and about 15 years later devs and techs still
-            comprise roughly 30% of all staff.
-            As a natural consequence, Mollie still is—and aims to remain—highly tech-minded. We take a developers-first
-            approach to our products and internal
-            development strategy, something that both our customers and our developers appreciate.
-          </div>
-        <br>
-        <br>
+      <br>
+      <br>
+      <div class="companyinformantion">
+        We provide a simple and clear payment API, which enables webshop and app builders to implement more than ten
+        different payment methods in one go.In the first
+        few years of existence, Mollie was a developer-only adventure and about 15 years later devs and techs still
+        comprise roughly 30% of all staff.
+        As a natural consequence, Mollie still is—and aims to remain—highly tech-minded. We take a developers-first
+        approach to our products and internal
+        development strategy, something that both our customers and our developers appreciate.
+      </div>
+      <br>
+      <br>
     </section>
+
+
+
+      <section>
+        <span>about company</span>
+        <br>
+        <br>
+        <div class="companyinformantion">
+          We provide a simple and clear payment API, which enables webshop and app builders to implement more than ten
+          different payment methods in one go.In the first
+          few years of existence, Mollie was a developer-only adventure and about 15 years later devs and techs still
+          comprise roughly 30% of all staff.
+          As a natural consequence, Mollie still is—and aims to remain—highly tech-minded. We take a developers-first
+          approach to our products and internal
+          development strategy, something that both our customers and our developers appreciate.
+        </div>
+        <br>
+        <br>
+      </section>
 
   </div>
 </template>
-
+      
 <style scoped>
 .companyinformantion {
-
   padding: 10px;
   background-color: rgba(242, 246, 248, 1);
   border-radius: 30px;
-  
 }
 
 .card {
   width: 500px;
-  height: 604px;
+  height: atuo;
   margin-bottom: 0;
-  margin-left:20%;
-  margin-top:10%;
+ 
   padding: 10px;
+  border-color: rgba(217, 217, 217, 1);
 }
 
 .box {
@@ -72,7 +96,25 @@
   line-height: 200px;
 }
 
+.stackbox{
+  margin-bottom:15px;
+}
+
+.stack {
+  display: inline-block;
+  background-color: rgba(242, 246, 248, 1);
+  border-radius: 10px;
+  color: rgba(247, 56, 89, 1);
+  margin-right: 5px;
+  padding-top: 4px;
+  padding-right: 10px;
+  padding-left: 10px;
+  padding-bottom: 4px;
+}
+
 .title {
   color: rgba(56, 66, 89, 1);
   font-size: 50px;
-}</style>
+}
+</style>
+      
