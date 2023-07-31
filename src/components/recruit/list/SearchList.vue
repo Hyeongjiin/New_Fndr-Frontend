3f771762cff2e2b4319f094ffe1b7be8497eaae6
박진희 <melbazzang@naver.com>
2023-07-31 20:05:16 +0900

    [Fix] SearchList.vue UI다시 수정 (임시 저장)

--- src/components/recruit/list/SearchList.vue
+++ src/components/recruit/list/SearchList.vue
@@ -1,54 +1,24 @@
 <template>
-
-    <div class="box1">
-
-        <div class="maintext">
-        Remote Client Specialist 
+    <base-card>
+        <div class="Search-section1">
+            <div class="info">
+                <h3>여기에 제목</h3>
+                <ul>
+                    <li>Agent Pronto</li>
+                    <li>remote</li>
+                    <li>visa support</li>
+                </ul>
+            </div>
+            <div class="info-img">
+                <img src="">
+            </div>
         </div>
-
-        <div class="middlebox">
-            <div style="margin-right: 15px;"> Agent Pronto  </div>
-            <div style="margin-right: 15px;">  reloction or remote  </div>
-            <div style="margin-right: 15px;"> visa support  </div>
-        </div>
-        
-        <div class="smallbox">
-        <h5 class="card-title">Design a scalable, stable and secure platform using cloud platforms, especially AWS, and key technologies such
-            as kubernetes and Kafka...</h5>
-        </div>
-      </div>
-    
+        <div class="description"></div>
+    </base-card>
 </template>
 
 <style scoped>
-
-    .box1{
-        
-        margin-left:3%;
-        margin-bottom:3%;
-        padding:30px;
-        width : 700px;
-        height: auto;
-        background-color: rgba(248, 248, 248, 1);
-        border-radius:  30px;;    
-    }
-        .maintext{
-        font-size: 30px;
-    }
-   .middlebox{
-    display: flex;
-   }
-  
-    .card-title{
-        font-size: 22px;
-    }
-    .smallbox{
-        border: 10px;
-        margin-top: 25px;
-        background-color: rgba(217, 217, 217, 1);
-        border-radius: 10px;
-        display:inline-block;
-        text-align: center;
-    }
-
+* {
+    font-size: 16px;
+}
 </style>
