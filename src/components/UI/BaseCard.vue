b862caa96f588f991e6d17e48e6eaf712e8abd81
박진희 <melbazzang@naver.com>
2023-08-08 14:06:45 +0900

    [Style] BaseCard 스타일 수정

--- src/components/UI/BaseCard.vue
+++ src/components/UI/BaseCard.vue
@@ -10,8 +10,9 @@ div {
   box-shadow: 0px 3px 8px 1px rgba(233, 233, 233, 0.75);
   padding: 2rem;
   border: #d9d9d9 1px solid;
-  margin: 0 auto;
+  /* margin: 0 auto; */
   margin-bottom: 20px;
-  max-width: 40rem;
+  background-color:#f8f8f8;
+  width: auto;
 }
 </style>
