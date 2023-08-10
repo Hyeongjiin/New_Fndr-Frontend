58e064efabbbba4f6ed9f427a705613a3c87502c
Hyeongjin <lipfolnp@gmail.com>
2023-08-10 17:59:19 +0900

    [Fix]main 페이지 조회시 데이터 한 번만 받아오도록 로직 수정

--- src/components/mainpage/MainPage.vue
+++ src/components/mainpage/MainPage.vue
@@ -22,7 +22,11 @@ export default {
                 'Accept': 'application/json',
             },
         }).then(response => response.json())
-            .then(data => console.log(this.articles = [...data.Remote, ...data.Visa]))
+            .then(data => {
+                this.articles.Remote = data.Remote;
+                this.articles.Visa = data.Visa;
+                console.log(this.articles)
+            })
             .catch(error => {
                 console.error('Error fetching data:', error);
             });
@@ -30,7 +34,10 @@ export default {
 
     data() {
         return {
-            articles: [],
+            articles: {
+                Remote: [],
+                Visa: [],
+            },
         };
     },
     methods: {
