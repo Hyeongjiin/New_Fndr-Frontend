65c985202281d283d78ae3db4bc2aa08fa45c2a0
박진희 <melbazzang@naver.com>
2023-08-10 23:35:39 +0900

    [Fix] 디테일뷰 tag, 본문내용 data binding

--- src/components/recruit/detail/DetailViewLeft.vue
+++ src/components/recruit/detail/DetailViewLeft.vue
@@ -1,115 +1,73 @@
 <template>
   <div class="card">
 
-    <h1 class="title"> {{ jobDetail.description_title }} </h1> 
-
+    <h1 class="title"> {{ jobDetail.description_title }} </h1>
     <div class="stackbox">
-      <div class="stack">Java</div>
-      <div class="stack">Python</div>
-      <div class="stack">c++</div>
-      <div class="stack">C#</div>
+      <div class="stack" v-for="tag in tagsArray" :key="tag">
+        {{ tag }}
+      </div>
     </div>
-
     <hr style="margin-top: 0; ">
-
     <section>
-      <span>about company</span>
-      <br>
-      <br>
-      <div class="companyinformantion">
-        We provide a simple and clear payment API, which enables webshop and app builders to implement more than ten
-        different payment methods in one go.In the first
-        few years of existence, Mollie was a developer-only adventure and about 15 years later devs and techs still
-        comprise roughly 30% of all staff.
-        As a natural consequence, Mollie still is—and aims to remain—highly tech-minded. We take a developers-first
-        approach to our products and internal
-        development strategy, something that both our customers and our developers appreciate.
+      <div class="info" v-html="jobDetail.description_content">
       </div>
-      <br>
-      <br>
     </section>
-
-
-    <section>
-      <span>about company</span>
-      <br>
-      <br>
-      <div class="companyinformantion">
-        We provide a simple and clear payment API, which enables webshop and app builders to implement more than ten
-        different payment methods in one go.In the first
-        few years of existence, Mollie was a developer-only adventure and about 15 years later devs and techs still
-        comprise roughly 30% of all staff.
-        As a natural consequence, Mollie still is—and aims to remain—highly tech-minded. We take a developers-first
-        approach to our products and internal
-        development strategy, something that both our customers and our developers appreciate.
-      </div>
-      <br>
-      <br>
-    </section>
-
-
-
-      <section>
-        <span>about company</span>
-        <br>
-        <br>
-        <div class="companyinformantion">
-          We provide a simple and clear payment API, which enables webshop and app builders to implement more than ten
-          different payment methods in one go.In the first
-          few years of existence, Mollie was a developer-only adventure and about 15 years later devs and techs still
-          comprise roughly 30% of all staff.
-          As a natural consequence, Mollie still is—and aims to remain—highly tech-minded. We take a developers-first
-          approach to our products and internal
-          development strategy, something that both our customers and our developers appreciate.
-        </div>
-        <br>
-        <br>
-      </section>
-
   </div>
 </template>
 
 <script>
 export default {
   props: ['jobDetail'],
+  computed: {
+    tagsArray() {
+      try {
+        return JSON.parse(this.jobDetail.tag);
+      } catch (error) {
+        console.error("Failed to parse jobDetail.tag", error);
+        return []; // 파싱에러 났을 때 빈 배열을 리턴함
+      }
+    }
+  }
 }
 </script>
 
 <style scoped>
-.companyinformantion {
-  padding: 10px;
-  background-color: rgba(242, 246, 248, 1);
-  border-radius: 30px;
-}
-
 .card {
-  width: 500px;
-  height: atuo;
+  width: 50rem;
+  height: auto;
   margin-bottom: 0;
- 
-  padding: 10px;
+  padding: 20px;
+  margin-right: 2rem;
   border-color: rgba(217, 217, 217, 1);
 }
 
+.info {
+  padding: 30px;
+  background-color: rgba(242, 246, 248, 1);
+  border-radius: 30px;
+  line-height: 200%;
+}
+
 .box {
-  width: 100px;
+  width: 70%;
   height: auto;
   margin-right: 3ch;
-  margin-left: 1ch;
   background-color: rgba(242, 246, 248, 1);
   border-color: rgba(56, 66, 89, 1);
   border-radius: 30px;
   line-height: 200px;
 }
 
-.stackbox{
-  margin-bottom:15px;
+.stackbox {
+  margin-bottom: 15px;
 }
 
 .stack {
   display: inline-block;
   background-color: rgba(242, 246, 248, 1);
-  border-radius: 10px;
+  border-radius: 20px;
+  font-size: 15px;
+  font-weight: 700;
   color: rgba(247, 56, 89, 1);
   margin-right: 5px;
   padding-top: 4px;
@@ -120,7 +78,7 @@ export default {
 
 .title {
   color: rgba(56, 66, 89, 1);
-  font-size: 50px;
+  font-size: 2rem;
 }
 </style>
       
